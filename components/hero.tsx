338566c8e73990ac8e7e21a78e4abaa3efbb9519
NextGennCodde <businessmy060@gmail.com>
2026-08-22 23:29:55 +0500

    update animation for hero section

--- components/hero.tsx
+++ components/hero.tsx
@@ -23,7 +23,7 @@ export function HeroSection() {
 
   return (
     <section className="relative w-full min-h-[100dvh] overflow-hidden bg-[var(--color-forest)]">
-      {/* Background image */}
+      {/* Background image check */}
       <div className="absolute inset-0">
         <Image
           src="/hero-homepage.png"
